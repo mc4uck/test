598d75c5ec9a3acc73ba3a9f880495fb7a34bba3
mc4uck <mc4uck@gmail.com>
2019-10-07 13:41:12 +0300

    Создан файл HelloWorld.java

--- src/com/mc4uck/tests/HelloWorld.java
+++ src/com/mc4uck/tests/HelloWorld.java
@@ -1,8 +1,5 @@
 package com.mc4uck.tests;
 
-/**
- * Created by mc4uck on 07.10.2019.
- */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
